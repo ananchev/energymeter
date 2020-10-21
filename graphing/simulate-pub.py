89f991d3c5b47c1e08d38a2d3c2ecaf1b62d357a
Anton Nanchev <ananchev@gmail.com>
2020-10-21 23:23:35 +0200

    simulate measurement from two lines

--- graphing/simulate-pub.py
+++ graphing/simulate-pub.py
@@ -9,13 +9,15 @@ def on_publish(client,userdata,result):
     print("data published")
     pass
 
-def pub(prev=0):
+def pub(prev=(0,0)):
     client1= paho.Client("control1")
     client1.on_publish = on_publish
     client1.connect(broker,port)
-    value = prev + random.randint(0,9)
-    ret = client1.publish("energymeter/line1", value)
-    return value   
+    value1 = prev[0] + random.randint(0,9)
+    value2 = prev[1] + random.randint(0,9)
+    ret1 = client1.publish("energymeter/line1", value1)
+    ret2 = client1.publish("energymeter/line2", value2)
+    return (value1, value2)   
 
 from apscheduler.events import EVENT_JOB_ERROR, EVENT_JOB_EXECUTED
 def listener(event):
